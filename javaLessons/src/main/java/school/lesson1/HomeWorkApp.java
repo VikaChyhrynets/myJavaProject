4de89118d4e43b616c53c79a9952a0269be0d5ef
Viktoryia Chyhrynets <vika.dolgikh@mail.ru>
2022-05-17 23:38:04 +0300

    Ready HW lesson_1

---
+++ javaLessons/src/main/java/school/lesson1/HomeWorkApp.java
@@ -0,0 +1,54 @@
+package school.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -20;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else {
+            if (value > 0 && value <= 100) {
+                System.out.println("Желтый");
+            } else {
+                if (value > 100) {
+                    System.out.println("Зеленый");
+                }
+            }
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = -5;
+        int b = 98;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            if (a < b) {
+                System.out.println("a < b");
+            }
+        }
+    }
+}
